6e080149e3d52534ab9bc7931ba18c4ce1bcd3e8
Geoff Hackett <ghackett@gmail.com>
2014-06-09 22:12:54 -0400

    add whats missing to TypedIntent interface

--- base/src/main/java/com/episode6/hackit/android/typed/intent/TypedIntent.java
+++ base/src/main/java/com/episode6/hackit/android/typed/intent/TypedIntent.java
@@ -1,7 +1,10 @@
 package com.episode6.hackit.android.typed.intent;
 
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Rect;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.episode6.hackit.android.typed.bundle.BundleKey;
@@ -16,19 +19,58 @@ public interface TypedIntent {
   <T extends Enum> T getAction(Class<T> actionEnumClass);
   String getActionString();
   TypedIntent setAction(Enum action);
+  TypedIntent setAction(String actionString);
 
+  boolean hasCategory(String category);
   Set<String> getCategories();
   TypedIntent addCategory(String category);
+  TypedIntent removeCategory(String category);
 
   ComponentName getComponent();
+  ComponentName resolveComponent();
+  TypedIntent setClass(Context packageContext, Class<?> cls);
+  TypedIntent setClassName(Context packageContext, String className);
+  TypedIntent setClassName(String packageName, String className);
+  TypedIntent setComponent(ComponentName component);
 
+  boolean hasData();
+  Uri getData();
+  String getDataString();
+  TypedIntent setData(Uri data);
+
+  int getFlags();
+  TypedIntent addFlags(int flags);
+  TypedIntent setFlags(int flags);
+
+  String getPackage();
+  TypedIntent setPackage(String packageString);
+
+  String getScheme();
+
+  Rect getSourceBounds();
+  TypedIntent setSourceBounds(Rect sourceBounds);
+
+  String getType();
+  String resolveType();
+  TypedIntent setType(String type);
+
+  boolean hasFileDescriptors();
+
+  TypedIntent setExtrasClassLoader(ClassLoader classLoader);
+
+  boolean hasExtra(BundleKey key);
   TypedBundle getExtras();
   TypedIntent putExtras(TypedBundle bundle);
   TypedIntent putExtras(Bundle bundle);
   TypedIntent putExtras(TypedIntent intent);
   TypedIntent putExtras(Intent intent);
+  TypedIntent replaceExtras(TypedBundle bundle);
+  TypedIntent replaceExtras(Bundle bundle);
+  TypedIntent replaceExtras(TypedIntent intent);
+  TypedIntent replaceExtras(Intent intent);
   <T> T getExtra(BundleKey<T> key);
   <T> TypedIntent putExtra(BundleKey<T> key, @Nullable T value);
+  TypedIntent removeExtra(BundleKey key);
 
   Intent getIntent();
 }
--- base/src/main/java/com/episode6/hackit/android/typed/intent/TypedIntentWrapperImpl.java
+++ base/src/main/java/com/episode6/hackit/android/typed/intent/TypedIntentWrapperImpl.java
@@ -1,7 +1,12 @@
 package com.episode6.hackit.android.typed.intent;
 
 import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Rect;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 
@@ -17,6 +22,8 @@ import javax.inject.Inject;
 
 public class TypedIntentWrapperImpl implements TypedIntentWrapper {
 
+  @Inject ContentResolver mContentResolver;
+  @Inject PackageManager mPackageManager;
   @Inject MapLikeTranslator mMapLikeTranslator;
   @Inject TypedBundleWrapper mTypedBundleWrapper;
   @Inject ActionTranslator mActionTranslator;
@@ -62,8 +69,18 @@ public class TypedIntentWrapperImpl implements TypedIntentWrapper {
 
     @Override
     public TypedIntent setAction(Enum action) {
-      getIntent().setAction(mActionTranslator.encodeAction(action));
+      return setAction(mActionTranslator.encodeAction(action));
+    }
+
+    @Override
+    public TypedIntent setAction(String actionString) {
+      getIntent().setAction(actionString);
       return this;
+    }
+
+    @Override
+    public boolean hasCategory(String category) {
+      return getIntent().hasCategory(category);
     }
 
     @Override
@@ -78,8 +95,140 @@ public class TypedIntentWrapperImpl implements TypedIntentWrapper {
     }
 
     @Override
+    public TypedIntent removeCategory(String category) {
+      getIntent().removeCategory(category);
+      return this;
+    }
+
+    @Override
     public ComponentName getComponent() {
       return getIntent().getComponent();
+    }
+
+    @Override
+    public ComponentName resolveComponent() {
+      return getIntent().resolveActivity(mPackageManager);
+    }
+
+    @Override
+    public TypedIntent setClass(Context packageContext, Class<?> cls) {
+      getIntent().setClass(packageContext, cls);
+      return this;
+    }
+
+    @Override
+    public TypedIntent setClassName(Context packageContext, String className) {
+      getIntent().setClassName(packageContext, className);
+      return this;
+    }
+
+    @Override
+    public TypedIntent setClassName(String packageName, String className) {
+      getIntent().setClassName(packageName, className);
+      return this;
+    }
+
+    @Override
+    public TypedIntent setComponent(ComponentName component) {
+      getIntent().setComponent(component);
+      return this;
+    }
+
+    @Override
+    public boolean hasData() {
+      return getData() != null;
+    }
+
+    @Override
+    public Uri getData() {
+      return getIntent().getData();
+    }
+
+    @Override
+    public String getDataString() {
+      return getIntent().getDataString();
+    }
+
+    @Override
+    public TypedIntent setData(Uri data) {
+      getIntent().setData(data);
+      return this;
+    }
+
+    @Override
+    public int getFlags() {
+      return getIntent().getFlags();
+    }
+
+    @Override
+    public TypedIntent addFlags(int flags) {
+      getIntent().addFlags(flags);
+      return this;
+    }
+
+    @Override
+    public TypedIntent setFlags(int flags) {
+      getIntent().setFlags(flags);
+      return this;
+    }
+
+    @Override
+    public String getPackage() {
+      return getIntent().getPackage();
+    }
+
+    @Override
+    public TypedIntent setPackage(String packageString) {
+      getIntent().setPackage(packageString);
+      return this;
+    }
+
+    @Override
+    public String getScheme() {
+      return getIntent().getScheme();
+    }
+
+    @Override
+    public Rect getSourceBounds() {
+      return getIntent().getSourceBounds();
+    }
+
+    @Override
+    public TypedIntent setSourceBounds(Rect sourceBounds) {
+      getIntent().setSourceBounds(sourceBounds);
+      return this;
+    }
+
+    @Override
+    public String getType() {
+      return getIntent().getType();
+    }
+
+    @Override
+    public String resolveType() {
+      return getIntent().resolveType(mContentResolver);
+    }
+
+    @Override
+    public TypedIntent setType(String type) {
+      getIntent().setType(type);
+      return this;
+    }
+
+    @Override
+    public boolean hasFileDescriptors() {
+      return getIntent().hasFileDescriptors();
+    }
+
+    @Override
+    public TypedIntent setExtrasClassLoader(ClassLoader classLoader) {
+      getIntent().setExtrasClassLoader(classLoader);
+      return this;
+    }
+
+    @Override
+    public boolean hasExtra(BundleKey key) {
+      return getIntent().hasExtra(key.getKeyString());
     }
 
     @Override
@@ -110,6 +259,28 @@ public class TypedIntentWrapperImpl implements TypedIntentWrapper {
     }
 
     @Override
+    public TypedIntent replaceExtras(TypedBundle bundle) {
+      return replaceExtras(bundle.getBundle());
+    }
+
+    @Override
+    public TypedIntent replaceExtras(Bundle bundle) {
+      getIntent().replaceExtras(bundle);
+      return this;
+    }
+
+    @Override
+    public TypedIntent replaceExtras(TypedIntent intent) {
+      return replaceExtras(intent.getIntent());
+    }
+
+    @Override
+    public TypedIntent replaceExtras(Intent intent) {
+      getIntent().replaceExtras(intent);
+      return this;
+    }
+
+    @Override
     public <T> T getExtra(BundleKey<T> key) {
       return mMapLikeTranslator.get(mIntent, key);
     }
@@ -117,6 +288,12 @@ public class TypedIntentWrapperImpl implements TypedIntentWrapper {
     @Override
     public <T> TypedIntent putExtra(BundleKey<T> key, @Nullable T value) {
       mMapLikeTranslator.set(mIntent, key, value);
+      return this;
+    }
+
+    @Override
+    public TypedIntent removeExtra(BundleKey key) {
+      getIntent().removeExtra(key.getKeyString());
       return this;
     }
 

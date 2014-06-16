c9e3bae50a354be9db49cb50a8a438fb68e70a43
Geoff Hackett <ghackett@gmail.com>
2014-06-16 12:46:08 -0400

    Provide a lowercase enum type adapter factory for gson, and offer a way to inject it without having to build the whole gson yourself

--- base/src/main/java/com/episode6/hackit/android/extmodule/GsonModule.java
+++ base/src/main/java/com/episode6/hackit/android/extmodule/GsonModule.java
@@ -1,7 +1,12 @@
 package com.episode6.hackit.android.extmodule;
 
 import com.episode6.hackit.android.app.scope.ApplicationScope;
+import com.google.common.collect.ImmutableSet;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapterFactory;
+
+import java.util.Set;
 
 import javax.inject.Singleton;
 
@@ -13,8 +18,17 @@ import dagger.Provides;
     includes = {ApplicationScope.class})
 public class GsonModule {
 
+  @Provides(type = Provides.Type.SET_VALUES) @Singleton
+  Set<TypeAdapterFactory> provideDefaultTypeAdapterFactorySet() {
+    return ImmutableSet.of();
+  }
+
   @Provides @Singleton
-  Gson provideGson() {
-    return new Gson();
+  Gson provideGson(Set<TypeAdapterFactory> typeAdapterFactories) {
+    GsonBuilder builder = new GsonBuilder();
+    for (TypeAdapterFactory factory : typeAdapterFactories) {
+      builder.registerTypeAdapterFactory(factory);
+    }
+    return builder.create();
   }
 }

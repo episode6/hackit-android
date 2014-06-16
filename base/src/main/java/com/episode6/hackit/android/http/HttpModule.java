4b239d0dc0ede41198b44455a96202db5677106b
Geoff Hackett <ghackett@gmail.com>
2014-06-16 14:07:05 -0400

    add optional http module

---
+++ base/src/main/java/com/episode6/hackit/android/http/HttpModule.java
@@ -0,0 +1,35 @@
+package com.episode6.hackit.android.http;
+
+import android.app.Application;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * This module is optional. It has a non-empty constructor to ensure it only gets added at the
+ * application level so it can be a true singleton and can initialize eager singletons
+ */
+@Module(
+    library = true)
+public class HttpModule {
+
+  private CookieSyncManager mCookieSyncManager;
+
+  public HttpModule(Application application) {
+    mCookieSyncManager = CookieSyncManager.createInstance(application);
+  }
+
+  @Provides @Singleton
+  CookieSyncManager provideDefaultCookieSyncManager() {
+    return mCookieSyncManager;
+  }
+
+  @Provides
+  CookieManager provideDefaultCookieManager() {
+    return CookieManager.getInstance();
+  }
+}

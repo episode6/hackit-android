006121ee7afecc497a176aef94b09581b018f411
Geoff Hackett <ghackett@gmail.com>
2014-06-18 10:06:19 -0400

    things that need a Camera probably also need the CameraInfo to go along with it

--- base/src/main/java/com/episode6/hackit/android/media/CameraProvider.java
+++ base/src/main/java/com/episode6/hackit/android/media/CameraProvider.java
@@ -8,30 +8,53 @@ import javax.annotation.Nullable;
 
 public class CameraProvider {
 
-  public Optional<Camera> getRearFacingCamera() {
+  public Optional<CameraAndInfo> getRearFacingCamera() {
     return Optional.fromNullable(findCameraFacing(Camera.CameraInfo.CAMERA_FACING_BACK));
   }
 
-  public Optional<Camera> getFrontFacingCamera() {
+  public Optional<CameraAndInfo> getFrontFacingCamera() {
     return Optional.fromNullable(findCameraFacing(Camera.CameraInfo.CAMERA_FACING_FRONT));
   }
 
-  public Optional<Camera> getAnyCamera() {
-    if (Camera.getNumberOfCameras() > 0) {
-      return Optional.fromNullable(Camera.open(0));
+  public Optional<CameraAndInfo> getAnyCamera() {
+    int numberOfCameras = Camera.getNumberOfCameras();
+    if (numberOfCameras <= 0) {
+      return Optional.absent();
     }
-    return Optional.absent();
+
+    Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
+    Camera.getCameraInfo(0, cameraInfo);
+    Camera camera = Camera.open(0);
+    return Optional.of(new CameraAndInfo(camera, cameraInfo));
   }
 
-  private @Nullable Camera findCameraFacing(int facing) {
+  private @Nullable CameraAndInfo findCameraFacing(int facing) {
     int numberOfCameras = Camera.getNumberOfCameras();
     Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
     for (int i = 0; i < numberOfCameras; i++) {
       Camera.getCameraInfo(i, cameraInfo);
       if (cameraInfo.facing == facing) {
-        return Camera.open(i);
+        return new CameraAndInfo(Camera.open(i), cameraInfo);
       }
     }
     return null;
+  }
+
+  public static class CameraAndInfo {
+    private final Camera mCamera;
+    private final Camera.CameraInfo mCameraInfo;
+
+    CameraAndInfo(Camera camera, Camera.CameraInfo info) {
+      mCamera = camera;
+      mCameraInfo = info;
+    }
+
+    public Camera getCamera() {
+      return mCamera;
+    }
+
+    public Camera.CameraInfo getCameraInfo() {
+      return mCameraInfo;
+    }
   }
 }

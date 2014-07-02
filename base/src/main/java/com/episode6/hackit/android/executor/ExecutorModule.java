5349fd7a22157d48496cb5d4fd1133cf1623701f
Geoff Hackett <ghackett@gmail.com>
2014-07-01 23:13:46 -0400

    Provide a ui thread executor in the executors module

--- base/src/main/java/com/episode6/hackit/android/executor/ExecutorModule.java
+++ base/src/main/java/com/episode6/hackit/android/executor/ExecutorModule.java
@@ -6,6 +6,8 @@ import android.os.Looper;
 import com.episode6.hackit.android.annotation.ForUiThread;
 import com.episode6.hackit.android.app.scope.ApplicationScope;
 
+import java.util.concurrent.Executor;
+
 import dagger.Module;
 import dagger.Provides;
 
@@ -18,5 +20,15 @@ public class ExecutorModule {
   @Provides @ForUiThread
   Handler provideUiHandler() {
     return new Handler(Looper.getMainLooper());
+  }
+
+  @Provides @ForUiThread
+  Executor provideUiThreadExecutor(final @ForUiThread Handler handler) {
+    return new Executor() {
+      @Override
+      public void execute(Runnable command) {
+        handler.post(command);
+      }
+    };
   }
 }

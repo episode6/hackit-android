12f3479afd77a4301859bd5e699c03f1419d4098
Geoff Hackett <ghackett@gmail.com>
2014-10-20 18:18:29 -0400

    Add some bg stuff to the ExecutorModule. Not sure it's the best way to go, since HandlerThread.getLooper() will block if called immediately after HandlerThread.start(), but hopefully it should be mitigated if the app using it injects it as an eager singleton.

--- base/src/main/java/com/episode6/hackit/android/executor/ExecutorModule.java
+++ base/src/main/java/com/episode6/hackit/android/executor/ExecutorModule.java
@@ -1,12 +1,16 @@
 package com.episode6.hackit.android.executor;
 
 import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.Looper;
 
+import com.episode6.hackit.android.annotation.ForBackgroundThread;
 import com.episode6.hackit.android.annotation.ForUiThread;
 import com.episode6.hackit.android.app.scope.ApplicationScope;
 
 import java.util.concurrent.Executor;
+
+import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
@@ -24,6 +28,28 @@ public class ExecutorModule {
 
   @Provides @ForUiThread
   Executor provideUiThreadExecutor(final @ForUiThread Handler handler) {
+    return new Executor() {
+      @Override
+      public void execute(Runnable command) {
+        handler.post(command);
+      }
+    };
+  }
+
+  @Provides @Singleton @ForBackgroundThread
+  HandlerThread provideWorkerThread() {
+    HandlerThread thread = new HandlerThread("worker_thread", Thread.MIN_PRIORITY);
+    thread.start();
+    return thread;
+  }
+
+  @Provides @Singleton @ForBackgroundThread
+  Handler providerWorkerHandler(@ForBackgroundThread HandlerThread workerThread) {
+    return new Handler(workerThread.getLooper());
+  }
+
+  @Provides @Singleton @ForBackgroundThread
+  Executor provideBackgroundExecutor(final @ForBackgroundThread Handler handler) {
     return new Executor() {
       @Override
       public void execute(Runnable command) {
